omnirom/android_frameworks_av

cameraservice: move client_package_name to data/misc
Change-Id: I0d244b81625fd9514000bea313a28ad60321333f

services/camera/libcameraservice/CameraService.cpp
@@ -2517,7 +2517,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/omni/client_package_name");
+    std::ofstream cpf("/data/misc/omni/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif

xNombre/android_frameworks_av-1

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/camera/libcameraservice/CameraService.cpp
@@ -2542,7 +2542,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/lineage/client_package_name");
+    std::ofstream cpf("/data/misc/lineage/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif